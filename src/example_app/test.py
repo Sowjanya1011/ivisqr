322101e3f0c35842e5362549c0b053f8be84f529
Your Name <you@example.com>
2022-06-18 17:29:44 +0530

    schedule db

--- src/example_app/test.py
+++ src/example_app/test.py
@@ -18,23 +18,27 @@ device_type = os.environ.get('DEVICE_TYPE')
 con=p.connect(host=host,user=user,password=password,database=database)
 cur=con.cursor()
 def get_device(device_id,site):
+    con.ping(reconnect = True)
     query = "select temp_range from devices where deviceId={} and siteId={} and deviceTypeId={}".format(device_id,site,device_type)
     cur.execute(query)
     res = cur.fetchone()
     return res
 def get_latlng(site):
+    con.ping(reconnect = True)
     query = "select latitude,longitude from sites where siteId={}".format(site)
     cur.execute(query)
     res = cur.fetchone()
     return res    
 
 def get_schedule(device_id,site):
-    query = "select start_time,end_time,asset_id from qr_schedule where device_id={} and site_id={}".format(device_id,site)
+    con.ping(reconnect = True)
+    query = "select start_time,end_time,asset_id from schedule where device_id={} and site_id={} and device_type={}".format(device_id,site,device_type)
     cur.execute(query)
     res = cur.fetchall()
     return res
 
 def get_asset(asset_id):
+    con.ping(reconnect = True)
     query = "select asset from assets where id={}".format(asset_id)
     print("assetid:", asset_id)
     cur.execute(query)
@@ -44,6 +48,7 @@ def get_asset(asset_id):
 
 
 def get_temp(lat,lng,rng):
+    con.ping(reconnect = True)
     day = time.strftime("%p")
     c =int(float(requests.get(url.format(lat,lng,key)).json()['current']['temp'])-273.15)
     
@@ -59,6 +64,7 @@ def get_temp(lat,lng,rng):
     return day+a
 
 def get_rule(rule):
+    con.ping(reconnect = True)
     query = "select id from rules where name='{}'".format(rule)
     cur.execute(query)
     res = cur.fetchone()[0]
@@ -68,6 +74,7 @@ def get_rule(rule):
 
 
 def get_assetId(device_id,rule_id,site):
+    con.ping(reconnect = True)
     query = "select asset from rule_engine where device_id={} and rule={} and device_type='{}' and site_id={}".format(device_id,rule_id,device_type,site)
     cur.execute(query)
     res = cur.fetchone()[0]
